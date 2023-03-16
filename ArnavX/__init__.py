50017d5674f1b7d8b602c8ecc8426d5925209466
ARNAV SINGH <116391676+Xdarnav@users.noreply.github.com>
2023-03-16 16:11:28 +0530

    Update __init__.py

--- ArnavX/__init__.py
+++ ArnavX/__init__.py
@@ -1,28 +1,31 @@
-from ArnavX.core.bot import ArnavXBot
+from ArnavX.core.bot import ArnavBot
 from ArnavX.core.dir import dirr
 from ArnavX.core.git import git
 from ArnavX.core.userbot import Userbot
 from ArnavX.misc import dbb, heroku, sudo
-from aiohttp import ClientSession
 
 from .logging import LOGGER
 
-
+# Directories
 dirr()
 
+# Check Git Updates
 git()
 
+# Initialize Memory DB
 dbb()
 
+# Heroku APP
 heroku()
 
+# Load Sudo Users from DB
 sudo()
 
-# Clients
-app = ArnavXBot()
+# Bot Client
+app = YukkiBot()
 
+# Assistant Client
 userbot = Userbot()
-
 
 from .platforms import *
 
