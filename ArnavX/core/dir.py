e3c6e993ee6c31c23d6b4235629f4d9bd622a1b8
ARNAV SINGH <116391676+Xdarnav@users.noreply.github.com>
2023-03-16 16:38:59 +0530

    Update dir.py

--- ArnavX/core/dir.py
+++ ArnavX/core/dir.py
@@ -6,19 +6,19 @@ from ..logging import LOGGER
 
 
 def dirr():
-    if "assets" not in listdir():
+    if "assets" not in listdir("ArnavX"):
         LOGGER(__name__).warning(
             f"Assets Folder not Found. Please clone repository again."
         )
         sys.exit()
-    for file in os.listdir():
+    for file in os.listdir(ArnavX):
         if file.endswith(".jpg"):
             os.remove(file)
     for file in os.listdir():
         if file.endswith(".jpeg"):
             os.remove(file)
-    if "downloads" not in listdir():
+    if "downloads" not in listdir(ArnavX):
         mkdir("downloads")
-    if "cache" not in listdir():
+    if "cache" not in listdir(ArnavX):
         mkdir("cache")
-LOGGER(__name__).info("Directories Updated.")
+    LOGGER(__name__).info("Directories Updated.")
