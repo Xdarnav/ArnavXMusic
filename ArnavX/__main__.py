193d45c44d69a8b952d87442fa62429097cf15f3
KaaL <99601120+KaalOS@users.noreply.github.com>
2023-03-26 18:16:17 +0530

    Update __main__.py

--- ArnavX/__main__.py
+++ ArnavX/__main__.py
@@ -45,7 +45,7 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("ArnavX.plugins." + all_module)
+        importlib.import_module("ArnavX.plugins" + all_module)
     LOGGER("ArnavX.plugins").info(
         "Necessary Modules Imported Successfully."
     )
