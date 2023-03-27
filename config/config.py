2088e058f8cd8ebc25a1027b4a065b19f96450dd
ARNAV SINGH <116391676+Xdarnav@users.noreply.github.com>
2023-03-27 12:27:18 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "5482561033").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Xdarnav/ArnavXMusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://justpaste.it/u/cute_arnavsingh")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
